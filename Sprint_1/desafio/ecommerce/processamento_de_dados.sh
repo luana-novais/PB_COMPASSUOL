a633cee10b9bde50ca62f92041eb36da08e503f8
Luana Novais <luana_novais@outlook.com.br>
2024-08-25 18:53:19 -0300

    Atualizando processamento de dados e relatórios

--- Sprint_1/desafio/ecommerce/processamento_de_dados.sh
+++ Sprint_1/desafio/ecommerce/processamento_de_dados.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
-#criei a pasta vendas
+# Criando o diretório vendas
 mkdir -p vendas
 
-#copiei o arquivo de vendas
+# Copiando o arquivo de vendas
 cp dados_de_vendas.csv vendas/
 
-#subdiretorio backup
+# Criando o subdiretório backup dentro de vendas
 mkdir -p vendas/backup
 
-#capturando a data atual e armazenando em uma variavel DATA
+# Capturando a data atual e armazenando em uma variável DATA
 DATA=$(date +%Y%m%d)
 
-#copiando o arquivo de vendas com a data atual 
+# Copiando o arquivo de vendas com a data atual
 cp vendas/dados_de_vendas.csv vendas/backup/dados-$DATA.csv
 
-#movendo o arquivo e renomeando 
+# Movendo o arquivo e renomeando
 mv vendas/backup/dados-$DATA.csv vendas/backup/backup-dados-$DATA.csv
- 
 
+# Criando o relatório txt
+data_sistema=$(date +"%Y/%m/%d %H:%M")
+primeira_data=$(head -n 2 vendas/backup/backup-dados-${DATA}.csv | tail -n 1 | cut -d ',' -f 1)
+ultima_data=$(tail -n 1 vendas/backup/backup-dados-${DATA}.csv | cut -d ',' -f 1)
+quantidade_itens=$(cut -d ',' -f 2 vendas/backup/backup-dados-${DATA}.csv | sort | uniq | wc -l)
+
+# Escrevendo no relatório
+echo "Data do sistema operacional: $data_sistema" > vendas/backup/relatorio.txt
+echo "Data do primeiro registro de venda: $primeira_data" >> vendas/backup/relatorio.txt
+echo "Data do último registro de venda: $ultima_data" >> vendas/backup/relatorio.txt
+echo "Quantidade total de itens diferentes vendidos: $quantidade_itens" >> vendas/backup/relatorio.txt
+
+# Mostrando as primeiras 10 linhas do arquivo de backup e adicionando ao relatório
+head -n 10 vendas/backup/backup-dados-${DATA}.csv >> vendas/backup/relatorio.txt
+
+# Comprimindo os arquivos e limpando os dados
+zip -r "vendas/backup/backup-dados-${DATA}.zip" "vendas/backup/backup-dados-${DATA}.csv"
+rm "vendas/backup/backup-dados-${DATA}.csv"
+rm vendas/dados_de_vendas.csv
