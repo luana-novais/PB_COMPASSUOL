560f84f385896fad593e3fd5ffe76a72c9decc34
Luana Novais <luana_novais@outlook.com.br>
2024-08-26 09:04:09 -0300

    Alteração do caminho dos diretorios, estava dando erro no agendamento

--- Sprint_1/desafio/ecommerce/processamento_de_dados.sh
+++ Sprint_1/desafio/ecommerce/processamento_de_dados.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
-# Criando o diretório vendas
-mkdir -p vendas
 
-# Copiando o arquivo de vendas
-cp dados_de_vendas.csv vendas/
+# Criação do diretório vendas
+mkdir -p /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas
 
-# Criando o subdiretório backup dentro de vendas
-mkdir -p vendas/backup
+# Cópia do arquivo de vendas
+cp /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/dados_de_vendas.csv /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/
 
-# Capturando a data atual e armazenando em uma variável DATA
+# Criação do subdiretório backup
+mkdir -p /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup
+
+# Captura da data atual
 DATA=$(date +%Y%m%d)
 
-# Copiando o arquivo de vendas com a data atual
-cp vendas/dados_de_vendas.csv vendas/backup/dados-$DATA.csv
+# Cópia do arquivo para o diretório de backup com a data atual
+cp /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/dados_de_vendas.csv /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup/dados-$DATA.csv
 
-# Movendo o arquivo e renomeando
-mv vendas/backup/dados-$DATA.csv vendas/backup/backup-dados-$DATA.csv
+# Renomeação do arquivo no backup
+mv /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup/dados-$DATA.csv /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup/backup-dados-$DATA.csv
 
-# Criando o relatório txt
+# Criação do relatório
 data_sistema=$(date +"%Y/%m/%d %H:%M")
-primeira_data=$(head -n 2 vendas/backup/backup-dados-${DATA}.csv | tail -n 1 | cut -d ',' -f 1)
-ultima_data=$(tail -n 1 vendas/backup/backup-dados-${DATA}.csv | cut -d ',' -f 1)
-quantidade_itens=$(cut -d ',' -f 2 vendas/backup/backup-dados-${DATA}.csv | sort | uniq | wc -l)
+primeira_data=$(tail -n +2 /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup/backup-dados-$DATA.csv | head -n 1 | cut -d ',' -f 5)
+ultima_data=$(tail -n 1 /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup/backup-dados-$DATA.csv | cut -d ',' -f 5)
+quantidade_itens=$(cut -d ',' -f 3 /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup/backup-dados-$DATA.csv | sort | uniq | wc -l)
 
-# Escrevendo no relatório
-echo "Data do sistema operacional: $data_sistema" > vendas/backup/relatorio.txt
-echo "Data do primeiro registro de venda: $primeira_data" >> vendas/backup/relatorio.txt
-echo "Data do último registro de venda: $ultima_data" >> vendas/backup/relatorio.txt
-echo "Quantidade total de itens diferentes vendidos: $quantidade_itens" >> vendas/backup/relatorio.txt
+echo "Data do sistema operacional: $data_sistema" > /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup/relatorio.txt
+echo "Data do primeiro registro de venda: $primeira_data" >> /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup/relatorio.txt
+echo "Data do último registro de venda: $ultima_data" >> /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup/relatorio.txt
+echo "Quantidade total de itens diferentes vendidos: $quantidade_itens" >> /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup/relatorio.txt
 
-# Mostrando as primeiras 10 linhas do arquivo de backup e adicionando ao relatório
-head -n 10 vendas/backup/backup-dados-${DATA}.csv >> vendas/backup/relatorio.txt
+# Adição das primeiras 10 linhas do CSV ao relatório
+head -n 10 /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup/backup-dados-$DATA.csv >> /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup/relatorio.txt
 
-# Comprimindo os arquivos e limpando os dados
-zip -r "vendas/backup/backup-dados-${DATA}.zip" "vendas/backup/backup-dados-${DATA}.csv"
-rm "vendas/backup/backup-dados-${DATA}.csv"
-rm vendas/dados_de_vendas.csv
+# Compressão do arquivo CSV
+zip /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup/backup-dados-$DATA.zip /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup/backup-dados-$DATA.csv
+
+# Remoção dos arquivos originais
+rm /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/backup/backup-dados-$DATA.csv
+rm /home/luana/trainee-repo-template/Sprint_1/desafio/ecommerce/vendas/dados_de_vendas.csv
